c9db3a628a6bfb874763e39a9d432fa8a62eae0e
BenLaverack <146448890+BenLaverack@users.noreply.github.com>
2023-11-16 16:19:18 +0000

    format changed and code working

--- cwk2/StepCounter_Final.c
+++ cwk2/StepCounter_Final.c
@@ -38,67 +38,67 @@ void tokeniseRecord(const char *input, const char *delimiter,
                     }
 
 
-
-
-// Complete the main function
 int main() {
     FITNESS_DATA fitness[100];
     int buffer_size=250;
     char line[buffer_size];
     int counter = 0;
-    char option[1];
-    int repeat = 1;
+    char option;
+    char name[100];
+    int total;
 
-    while (repeat = 1){
-        printf("Menu Options:\nA: Specify the filename to be imported\nB: Display the total number of records in the file\nC: Find the date and time of the timeslot with the fewest steps\nD: Find the date and time of the timeslot with the largest number of steps\nE: Find the mean step count of all the records in the file\nF: Find the longest continuous period where the step count is above 500 steps\nQ: Quit\nEnter choice: ");
-        scanf("%s", option);
+    while (1){
+        printf("Menu Options:\n");
+        printf("A: Specify the filename to be imported\n");
+        printf("B: Display the total number of records in the file\n");
+        printf("C: Find the date and time of the timeslot with the fewest steps\n");
+        printf("D: Find the date and time of the timeslot with the largest number of steps\n");
+        printf("E: Find the mean step count of all the records in the file\n");
+        printf("F: Find the longest continuous period where the step count is above 500 steps\n");
+        printf("Q: Quit\n");
+        printf("Enter choice: ");
 
-        if (strcmp(option, "A") == 0){
-            printf("A\n");
-            break;
-        }
-        else if (strcmp(option, "B") == 0){
-            printf("B\n");
-            break;
-        }
-        else if (strcmp(option, "C") == 0){
-            printf("C\n");
-            break;
-        }
-        else if (strcmp(option, "D") == 0){
-            printf("D\n");
-            break;
-        }
-        else if (strcmp(option, "E") == 0){
-            printf("E\n");
-            break;
-        }
-        else if (strcmp(option, "F") == 0){
-            printf("F\n");
-            break;
-        }
-        else if (strcmp(option, "Q") == 0){
-            printf("Q\n");
-            break;
-        }
-        else {
-            printf("Invalid choice. Try again.\n");
-        }
+        option = getchar();
 
+        switch(option)
+        {
+            case 'a':
+            case 'A':
+                printf("Enter the filename to be imported: ");
+                scanf("%s", name);
 
-    FILE *input=fopen("FitnessData_2023.csv","r");
-    char date[11];
-    char time[6];
-    char steps[10];
+                FILE *input=fopen(name,"r");
+                if (!input){
+                    printf("Invalid file name detected ");
+                }
+                else {
+                    printf("Filename entered correctly, please choose an option:\n");
+                }
+                char date[11];
+                char time[6];
+                char steps[10];
 
-    while(fgets(line, buffer_size, input)){
-        tokeniseRecord(line,",",date, time, steps);
+                while(fgets(line, buffer_size, input))
+                {
+                    tokeniseRecord(line,",",date, time, steps);
 
-        strcpy(fitness[counter].date, date);
-        strcpy(fitness[counter].time, time);
-        fitness[counter].steps = atoi(steps);
-        counter++;
-    }
+                    strcpy(fitness[counter].date, date);
+                    strcpy(fitness[counter].time, time);
+                    fitness[counter].steps = atoi(steps);
+                    counter++;
+                    total++;
+                }
+                break;
+
+            case 'q':
+            case 'Q':
+                return 0;
+                break;
+
+            default:
+                printf("Invalid Choice\n");
+                break;
+        }
     }
 
     
